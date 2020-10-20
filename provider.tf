26489dddcfc5ac908bcf152ee71b9e901fa7e30c
EC2 Default User <ec2-user@ip-172-31-32-208.ap-south-1.compute.internal>
2020-10-20 17:05:26 +0000

    first commit

---
+++ provider.tf
@@ -0,0 +1,13 @@
+provider "aws" {
+    region = "ap-south-1"
+    
+}
+# create vpc
+resource "aws_vpc" "main" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "main"
+  }
+}
